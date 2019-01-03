00e6edf5d164755ee8d737d2a3607a4c164b97b7
Dennis Duan <dduan97@gmail.com>
2019-01-02 22:59:31 -0500

    adding OF rules

--- pvv_switch.py
+++ pvv_switch.py
@@ -1,45 +1,40 @@
-"""
-    Packet headers used for PVV:
-        - vlan_vid for PVV vector
-        - vlan_pcp for current protocol tag
-
-    Need switches to support NX extensions as well as OF 1.2 or later
-
-    One edge case:
-        - packet comes in with PVV=011 and current_cp=2
-        - t0 matches and forwards to control plane 0, setting PVV=101 and current_cp=0
-        - control plane 0 is unavailable, sending packet to failure table
-        - failure table matches on PVV=111 and current_cp=0, but no protocols are available
-"""
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER, HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.mac import haddr_to_bin
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
-from ryu.controller import dpset
 
 
-
-class PVVSwitch(app_manager.RyuApp):
+class SimpleSwitch13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
+    PVV_TABLE = 1
+    FAILURE_TABLE = 200
+    # control plane 0 is OF, starting at table 2
+    # control plane 1 is OSPF starting at table 100
+    CP_TABLES = [2, 100]
 
     def __init__(self, *args, **kwargs):
-        super(PVVSwitch, self).__init__(*args, **kwargs)
-
-
-
-    # @set_ev_cls(ofp_event.EventOFPPacketIn, [MAIN_DISPATCHER, DEAD_DISPATCHER, HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER])
-    # def _packet_in_handler(self, ev):
-        # msg = ev.msg
-        # datapath = msg.datapath
-        # ofproto = datapath.ofproto
-
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
 
     @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER, HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER])
     def _state_change_handler(self, ev):
@@ -55,21 +50,94 @@ class PVVSwitch(app_manager.RyuApp):
             print "config dispatcher"
 
 
-    @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
-    def _port_status_handler(self, ev):
-        msg = ev.msg
-        reason = msg.reason
-        port_no = msg.desc.port_no
 
-        ofproto = msg.datapath.ofproto
-        if reason == ofproto.OFPPR_ADD:
-            self.logger.info("port added %s", port_no)
-        elif reason == ofproto.OFPPR_DELETE:
-            self.logger.info("port deleted %s", port_no)
-        elif reason == ofproto.OFPPR_MODIFY:
-            self.logger.info("port modified %s", port_no)
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
         else:
-            self.logger.info("Illeagal port state %s %s", port_no, reason)
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
 
     # preferences is a list of control planes in order
     # availabilities is an integer representation of available protocols
@@ -85,7 +153,7 @@ class PVVSwitch(app_manager.RyuApp):
             for pvv_match in xrange(2**len(preferences)):
 
                 match = ofproto_parser.OFPMatch(
-                    vlan_vid=pvv_match,
+                    vlan_vid=(0x1000 | pvv_match),
                     vlan_pcp=current_cp)
 
                 usable = pvv_match | availabilities
@@ -102,42 +170,39 @@ class PVVSwitch(app_manager.RyuApp):
                         if len(best_cps) == 2:
                             break
 
-                t0_actions = []
+                t1_actions = []
                 failure_actions = []
                 to_controller_action = ofproto_parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)
 
                 if len(best_cps) == 0:
                     # all flow rules should just be to send to controller
-                    t0_actions.append(to_controller_action)
+                    t1_actions.append(to_controller_action)
                     failure_actions.append(to_controller_action)
 
 
                 else:
-                    t0_cp = best_cps[0]
+                    t1_cp = best_cps[0]
 
                     # if the best_cp is NOT the same as current_cp, then we need to also rewrite the
                     # PVV/current_cp tags
-                    if t0_cp != current_cp:
-                        mask = 1 << t0_cp
+                    if t1_cp != current_cp:
+                        mask = 1 << t1_cp
                         new_pvv = pvv_match | mask
-                        t0_actions.append(
-                                ofproto_parser.OFPActionSetField(vlan_vid=new_pvv)
+                        t1_actions.append(
+                                ofproto_parser.OFPActionSetField(vlan_vid=(0x1000 | new_pvv))
                                 )
-                        t0_actions.append(
-                                ofproto_parser.OFPActionSetField(vlan_pcp=t0_cp)
+                        t1_actions.append(
+                                ofproto_parser.OFPActionSetField(vlan_pcp=t1_cp)
                                 )
 
 
-                    # flow rule action should be to forward to the table for t0_cp
-                    # since table 0 is reserved for PVV, control plane k will have table
-                    # k+1 (for now)
-                    t0_actions.append(ofproto_parser.NXActionResubmitTable(table_id=t0_cp+1))
+                    t1_actions.append(ofproto_parser.NXActionResubmitTable(table_id=self.CP_TABLES[t1_cp]))
 
                     if len(best_cps) == 1:
                         failure_actions.append(to_controller_action)
                     else:
                         failure_cp = best_cps[1]
-                        failure_actions.append(ofproto_parser.NXActionResubmitTable(table_id=failure_cp+1))
+                        failure_actions.append(ofproto_parser.NXActionResubmitTable(table_id=self.CP_TABLES[failure_cp]))
 
                         # if the failure table action goes to a different control plane from current_cp,
                         # then we need to rewrite the PVV headers
@@ -145,29 +210,27 @@ class PVVSwitch(app_manager.RyuApp):
                             mask = 1 << failure_cp
                             new_pvv = pvv_match | mask
                             failure_actions.append(
-                                    ofproto_parser.OFPActionSetField(vlan_vid=new_pvv)
+                                    ofproto_parser.OFPActionSetField(vlan_vid=(0x1000 | new_pvv))
                             )
                             failure_actions.append(
                                     ofproto_parser.OFPActionSetField(vlan_pcp=failure_cp)
                             )
 
-                # now we send teh message to the datapath. table id 0 is reserved for t0
-                t0_mod = ofproto_parser.OFPFlowMod(
+                # now we send teh message to the datapath. table id 0 is reserved for t1
+                t1_mod = ofproto_parser.OFPFlowMod(
                     datapath,
-                    table_id=0,
+                    table_id=self.PVV_TABLE,
                     command=datapath.ofproto.OFPFC_ADD,
                     match=match,
-                    instructions=[ofproto_parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions=t0_actions)])
+                    instructions=[ofproto_parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions=t1_actions)])
 
-                # for now, let's say the failure table is id 255
                 failure_mod = ofproto_parser.OFPFlowMod(
                     datapath,
-                    table_id=255,
+                    table_id=self.FAILURE_TABLE,
                     command=datapath.ofproto.OFPFC_ADD,
                     match=match,
                     instructions=[ofproto_parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions=failure_actions)])
 
-                print "sending message for t0 and failure tables"
-                datapath.send_msg(t0_mod)
+                print "sending message for t1 and failure tables"
+                datapath.send_msg(t1_mod)
                 datapath.send_msg(failure_mod)
-
